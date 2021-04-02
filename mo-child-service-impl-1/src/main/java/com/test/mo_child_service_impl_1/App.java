1695011c21d6516891840b54d37fe68f5d6749bc
Mohanraj Nagasamy <Mohanraj.Nagasamy@octanner.com>
2021-04-02 09:35:42 -0600

    init mutli project - slate

---
+++ mo-child-service-impl-1/src/main/java/com/test/mo_child_service_impl_1/App.java
@@ -0,0 +1,13 @@
+package com.test.mo_child_service_impl_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mo-child-service-impl-2/src/main/java/com/test/mo_child_service_impl_2/App.java
@@ -0,0 +1,13 @@
+package com.test.mo_child_service_impl_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
